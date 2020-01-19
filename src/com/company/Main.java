49b456023f2071d726edb0867db584b48acf62ce
avap8749 <34437704+avap8749@users.noreply.github.com>
2020-01-19 00:58:12 -0800

    removed all stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,41 +9,7 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	// write your code here
-        int[] i = {1,2,3,4,4};
-
-        List<String> l = Files.readAllLines(Paths.get("C:\\Temp\\a.txt"));
-
-        for (String s:l)
-            System.out.println(s);
-
-        String[] s = {"aaa", "bbb", "ccc"};
-        for (String str:s)
-            System.out.println(str);
-
-
-
-        try (InputStream is = new FileInputStream("C:\\Temp\\pic.jpeg");
-                OutputStream os = new FileOutputStream("C:\\Temp\\pic2.jpeg");
-                MyAutoClosable mac = new MyAutoClosable()) {
-            int val;
-            byte[] b = new byte[10];
-            while ((val = is.read(b)) > 0){
-                os.write(b, 0, val);
-                System.out.print(b);
-            }
-        }
-        catch (FileNotFoundException fnfe)  {
-            throw  fnfe;
-        }
-        System.out.print("done");
-
-        Base.print();
-        Child.print();
-
-        (new Base()).printObj();
-        (new Child()).printObj();
+    public static void main(String[] args) {
+	System.out.print("Hello world!");
     }
 }
-
